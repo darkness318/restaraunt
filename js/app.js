53399f188d958a2c4b6799da411258d0feb6c86d
Antônio Francisco <protectorsuperior392@gmail.com>
2024-11-05 16:27:50 -0400

    dark

--- js/app.js
+++ js/app.js
@@ -1,69 +1,78 @@
 $(document).ready(function () {
-    cardapio.eventos.init();
-//     console.log('Ola turma')
-})
+  cardapio.eventos.init();
+  //     console.log('Ola turma')
+});
 
 var cardapio = {};
 
 cardapio.eventos = {
-
-    init: () => {
-        // console.log('iniciou')
-        cardapio.metodos.obterItensCardapio();
-    }
-}
+  init: () => {
+    // console.log('iniciou')
+    cardapio.metodos.obterItensCardapio();
+  },
+};
 
 cardapio.metodos = {
+  //obtem a lista de itens do cardápio
+  obterItensCardapio: (categoria = "burgers", vermais = false) => {
+    var filtro = MENU[categoria];
+    console.log(filtro);
+    if (!vermais) {
+      $("#itensCardapio").html("");
+      $("#btnVerMais").removeClass("hidden");
+    }
+    //$("#itensCardapio").html('')
 
-    //obtem a lista de itens do cardápio
-    obterItensCardapio: (categoria = 'burgers', vermais = false) => {
-        var filtro = MENU[categoria]
-        console.log(filtro)
-        if(!vermais){
-            $("#itensCardapio").html('')
-            $("#btnVerMais").removeClass('hidden');
-        }
-        //$("#itensCardapio").html('')
-        
-        $.each(filtro, (i, e) => {
-            console.log(e.name);
-            // let temp = cardapio.templates.item;
-            let temp = cardapio.templates.item.replace(/\${img}/g, e.img)
-            .replace(/\${name}/g, e.name)
-            .replace(/\${price}/g, e.price.toFixed(2).replace('.',','))
+    $.each(filtro, (i, e) => {
+      console.log(e.name);
+      // let temp = cardapio.templates.item;
+      let temp = cardapio.templates.item
+        .replace(/\${img}/g, e.img)
+        .replace(/\${name}/g, e.name)
+        .replace(/\${id}/g, e.id)
+        .replace(/\${price}/g, e.price.toFixed(2).replace(".", ","));
 
-           //botão ver mais foi clicado (12 itens)
-            if(vermais && i >= 8 && i < 12){
-                $("#itensCardapio").append(temp)
-            }
-            //paginação inicial (8 itens)
-            if(!vermais && i < 8){
-                $("#itensCardapio").append(temp)
-            }
-            // $("#itensCardapio").append(temp)
-        })
+      //botão ver mais foi clicado (12 itens)
+      if (vermais && i >= 8 && i < 12) {
+        $("#itensCardapio").append(temp);
+      }
+      //paginação inicial (8 itens)
+      if (!vermais && i < 8) {
+        $("#itensCardapio").append(temp);
+      }
+      // $("#itensCardapio").append(temp)
+    });
 
-        //remove o ativo
-        $(".container-menu a").removeClass('active');
+    //remove o ativo
+    $(".container-menu a").removeClass("active");
 
-        //seta o menu para ativo
-        $("#menu-"+categoria).addClass('active')
+    //seta o menu para ativo
+    $("#menu-" + categoria).addClass("active");
+  },
 
-    },
+  //clique no botão ver mais
+  verMais: () => {
+    var ativo = $(".container-menu a.active").attr("id").split("menu-")[1]; // [menu-][burgers]
+    cardapio.metodos.obterItensCardapio(ativo, true);
 
-    //clique no botão ver mais
-    verMais: () => {
-        var ativo = $(".container-menu a.active").attr('id').split('menu-')[1]; // [menu-][burgers]
-        cardapio.metodos.obterItensCardapio(ativo, true);
-
-        $("#btnVerMais").addClass('hidden');
-    },
-}
+    $("#btnVerMais").addClass("hidden");
+  },
+  diminuirQuantidade: (id) => {
+    let qntdAtual = parseInt($("#qntd-carrinho-" + id).text());
+    if (qntdAtual > 0) {
+      $("#qntd-carrinho-" + id).text(qntdAtual - 1);
+    }
+  },
+  aumentarQuantidade: (id) => {
+    let qntdAtual = parseInt($("#qntd-carrinho-" + id).text());
+    $("#qntd-carrinho-" + id).text(qntdAtual + 1);
+  },
+};
 
 cardapio.templates = {
-    item: `
+  item: `
     <div class="col-3 mb-5">
-        <div class="card card-item">
+        <div class="card card-item" id="\${id}">
         <div class="img-produto">
             <img src="\${img}" />
         </div>
@@ -75,12 +84,11 @@ cardapio.templates = {
         </p>
 
         <div class="add-carrinho">
-            <span class="btn-menos"><i class="fas fa-minus"></i></span>
-            <span class="btn-numero-itens">0</span>
-            <span class="btn-mais"><i class="fas fa-plus"></i></span>
+            <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidade('\${id}')"><i class="fas fa-minus"></i></span>
+            <span class="btn-numero-itens" id="qntd-carrinho-\${id}">0</span>
+            <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidade('\${id}')"><i class="fas fa-plus"></i></span>
             <span class="btn btn-add"><i class="fa fa-shopping-bag"></i></span>
         </div>
         </div>
-    </div>`
-
-}
+    </div>`,
+};
